12deae19daf76c362b2561233a76e6efd51b7dd5
ArturPHD <artur.radzivilov@vilniustech.lt>
2025-09-16 13:22:10 +0200

    Implement 5-channel model with weight preservation

--- models/custom/models/adjacency_channels/model.py
+++ models/custom/models/adjacency_channels/model.py
@@ -16,64 +16,39 @@ class LazyConv(nn.Module):
 
 
 class AdjacencyChannelsYOLOModel(DetectionModel):
-    def __init__(self, cfg='yolov12.yaml', ch=3, nc=None, verbose=True, zero_new_weights=False):
+    def __init__(self, cfg='yolov12.yaml', ch=3, nc=None, verbose=True):
         super().__init__(cfg, ch, nc, verbose)
 
         first_layer = self.model[0]
-
-        if isinstance(first_layer, Conv):
-            new_conv = nn.Conv2d(
-                in_channels=5,
-                out_channels=first_layer.conv.out_channels,
-                kernel_size=first_layer.conv.kernel_size,
-                stride=first_layer.conv.stride,
-                padding=first_layer.conv.padding,
-                bias=False
-            )
-
-            original_weights = torch.randn(
-                first_layer.conv.out_channels,
-                3,
-                *first_layer.conv.kernel_size
-            )
-
-
-            if zero_new_weights:
-                new_weights = torch.zeros(
-                    first_layer.conv.out_channels,
-                    2,  # New 2 channels
-                    *first_layer.conv.kernel_size
-                )
-            else:
-                new_weights = torch.randn(
-                    first_layer.conv.out_channels,
-                    2,
-                    *first_layer.conv.kernel_size
-                ) * 1e-6
-
-            combined_weights = torch.cat((original_weights, new_weights), dim=1)
-
-            new_conv.weight.data.copy_(combined_weights)
-
-            new_conv_block = Conv(
-                c1=5,
-                c2=first_layer.conv.out_channels,
-                k=first_layer.conv.kernel_size,
-                s=first_layer.conv.stride,
-                p=first_layer.conv.padding,
-                act=first_layer.act
-            )
-
-            new_conv_block.conv = new_conv
-            new_conv_block.bn = first_layer.bn
-            new_conv_block.act = first_layer.act
-
-            new_conv_block.i = first_layer.i
-            new_conv_block.f = first_layer.f
-
-            self.model[0] = new_conv_block
-        else:
+        if not isinstance(first_layer, Conv):
             raise TypeError("The model's first layer is not a standard ultralytics Conv block.")
+
+        old_conv = first_layer.conv
+
+        if old_conv.in_channels == 5:
+            print("Model is already 5-channel. Skipping modification.")
+            return
+
+        print("Patching the first Conv layer to accept 5-channel input while preserving RGB weights.")
+
+        # Create a new 5-channel convolutional layer
+        new_conv = nn.Conv2d(
+            in_channels=5,
+            out_channels=old_conv.out_channels,
+            kernel_size=old_conv.kernel_size,
+            stride=old_conv.stride,
+            padding=old_conv.padding,
+            bias=(old_conv.bias is not None)
+        )
+
+        with torch.no_grad():
+            new_conv.weight.data[:, :3, :, :] = old_conv.weight.data.clone()
+            new_conv.weight.data[:, 3:, :, :] = torch.randn_like(new_conv.weight.data[:, 3:, :, :]) * 1e-6
+
+            if new_conv.bias is not None:
+                new_conv.bias.data = old_conv.bias.data.clone()
+
+        first_layer.conv = new_conv
 
     def warmup(self, imgsz=(1, 3, 640, 640)):
         """
